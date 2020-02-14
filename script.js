826022addd5be3a645558618cbc9cb5ad36a86ce
heyunnnnhao <heyunhaotq@yahoo.com>
2020-02-14 01:15:34 -0500

    update

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-var nothingYet = document.querySelector('#first');
-nothingYet.style.display = 'none';
+var notice = document.querySelector('#notice');
+notice.style.display = 'none';
+
+
+var coming = document.querySelector('#but');
+coming.onclick = function(){
+    document.querySelector('#coming').remove();
+    document.querySelector('#but').remove();
+};
