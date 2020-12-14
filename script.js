5bb509c8842fd94ee09f4bfc66d6abc31a13061f
heyunnnnhao <heyunhaotq@outlook.com>
2020-12-15 03:22:02 +0800

    update

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-window.onload = function () {
-  var name = document.getElementById('myname');
-  var info = document.getElementById('info');
+var myName = document.getElementById('myname');
+var info = document.getElementById('info');
 
-  name.onclick = function () {
-    info.classList.remove('info');
-  };
-
-  name.onmouseleave = function () {
-    setTimeout(function () {
-      info.classList.add('info');
-    }, 3000);
-  };
-};
+myName.onclick = () => info.classList.remove('info');
+myName.onmouseleave = () => setTimeout(() => info.classList.add('info'), 3000);
