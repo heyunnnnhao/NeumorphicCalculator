dc5422f3158cf1e19229001f6d7c190fa9416ea4
heyunnnnhao <heyunhaotq@outlook.com>
2020-12-15 21:58:47 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var myName = document.getElementById('myname');
-var myInfo = document.getElementById('info');
-myName.onclick = function () { return myInfo.classList.remove('info'); };
-myName.onmouseleave = function () { return setTimeout(function () { return myInfo.classList.add('info'); }, 3000); };
+"use strict";
+const myName = document.getElementById('myname');
+const myInfo = document.getElementById('info');
+myName.onclick = () => myInfo.classList.remove('info');
+myName.onmouseleave = () => setTimeout(() => myInfo.classList.add('info'), 3000);
