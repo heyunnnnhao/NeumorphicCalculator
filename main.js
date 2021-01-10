6d92c75baa44c103b7c61035276c671ea21d9de4
heyunnnnhao <heyunhaotq@outlook.com>
2021-01-11 02:18:38 +0800

    update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
+
 const myName = document.getElementById('myname');
 const myInfo = document.getElementById('info');
+
 myName.onclick = () => myInfo.classList.remove('info');
-myName.onmouseleave = () =>
-    setTimeout(() => myInfo.classList.add('info'), 3000);
+myName.onmouseleave = () => setTimeout(() => myInfo.classList.add('info'), 3000);
